465fe7b076e9d5aa11027df6f82a322a66d80689
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-06-04 10:39:32 -0300

    test: add integrations test to signup and signin

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import userController from '@/controllers/users-controller';
+import { validateBody } from '@/middlewares';
+import { signInSchema, signUpSchema } from '@/schemas';
 
 const userRouter = Router();
 
-userRouter.post('/sign-up', userController.createUser);
-userRouter.post('/sign-in', userController.singIn);
+userRouter.post('/sign-up',validateBody(signUpSchema), userController.createUser);
+userRouter.post('/sign-in',validateBody(signInSchema), userController.singIn);
 
 export default userRouter;
